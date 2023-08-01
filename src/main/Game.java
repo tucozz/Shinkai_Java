f2e5d1f3a50466f5b38518a1fd19354dd2fe5f0f
Tucozz <tutuestefanato@gmail.com>
2023-08-01 01:27:38 -0300

    add Game Loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,14 +1,50 @@
 package main;
 
-public class Game {
+public class Game implements Runnable{
     
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
 
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long LastFrame = System.nanoTime();
+        long now = System.nanoTime();
+
+        int frames = 0;
+        long LastCheck = System.currentTimeMillis();
+
+        while(true){
+
+            now = System.nanoTime();
+            if(now - LastFrame >= timePerFrame){
+                gamePanel.repaint();
+                LastFrame = now;
+                frames++;
+            }
+
+            if(System.currentTimeMillis() - LastCheck >= 1000){
+                LastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+
     }
 
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -11,7 +12,10 @@ import inputs.MouseInputs;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int dx = 100, dy = 100;
+    private float dx = 100, dy = 100;
+    private float xdir = 0.6f, ydir = 0.6f;
+    private Color color = new Color(150, 20, 90);
+    private Random random = new Random();
     
     public GamePanel(){
 
@@ -24,24 +28,45 @@ public class GamePanel extends JPanel {
 
     public void changedx(int val){
         this.dx += val;
-        repaint();
     }
 
     public void changedy(int val){
         this.dy += val;
-        repaint();
     }
 
     public void setRectPos(int x, int y){
         this.dx = x;
         this.dy = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.fillRect(dx, dy, 200, 50);
+        updateRect();
+
+        g.setColor(color);
+        g.fillRect((int)dx, (int)dy, 200, 50);
     }
 
+    private void updateRect(){
+        dx += xdir;
+        if(dx > 400 || dx < 0){
+            xdir *= -1;
+            color = getRDMColor();
+        }
+        dy += ydir;
+        if(dy > 400 || dy < 0){
+            ydir *= -1;
+            color = getRDMColor();
+        }
+
+    }
+
+    private Color getRDMColor(){
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r, g, b);
+    }
 }
