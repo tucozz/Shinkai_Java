0613292ec7582543f566271b34734e6d73b74f23
Tucozz <tutuestefanato@gmail.com>
2023-08-02 16:39:31 -0300

    Fix Constants

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,6 +11,7 @@ import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+import static utils.Constants.PlayerConstants.*;
 
 public class GamePanel extends JPanel {
 
@@ -20,16 +21,6 @@ public class GamePanel extends JPanel {
     private BufferedImage[][] animations;
     private int aniTick, aniIndex, aniSpeed = 15;
     private int player_action = IDLE;
-
-    public static final int IDLE = 0;
-    public static final int RUNNING = 1;
-    public static final int JUMP = 2;
-    public static final int FALLING = 3;
-    public static final int GROUND = 4;
-    public static final int HIT = 5;
-    public static final int ATTACK_1 = 6;
-    public static final int ATTACK_JUMP_1 = 7;
-    public static final int ATTACK_JUMP_2 = 8;
     
     public GamePanel(){
 
@@ -90,27 +81,6 @@ public class GamePanel extends JPanel {
                 aniIndex = 0;
             }
         }
-    }
-
-    public static int GetSpriteAmount(int player_action){
-        switch (player_action) {
-            case RUNNING:
-                return 0;
-            case IDLE:
-                return 5;
-            case HIT:
-                return 4;
-            case JUMP:
-            case ATTACK_1:
-            case ATTACK_JUMP_1:
-            case ATTACK_JUMP_2:
-                return 3;
-            case GROUND:
-                return 2;
-            case FALLING:
-            default:
-                return 1;
-         }
     }
 
     public void paintComponent(Graphics g){
